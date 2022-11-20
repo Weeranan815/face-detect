4f7a14a5141a3d46d756a7404634f03dac730634
MaxEiei_ <118649599+Weeranan815@users.noreply.github.com>
2022-11-20 12:40:53 +0700

    Update face.py

--- face.py
+++ face.py
@@ -1,7 +1,7 @@
 
 import cv2  
 img = cv2.imread('img.jpg')#เเปลงจาก ImagePath ให้ใส่ในตัวเเปล img
-face_cascade = cv2.CascadeClassifier('front_face.xml')#เเปลงตรวจจับใบหน้าให้อยู่ใน face_cascade
+face_cascade = cv2.CascadeClassifier('Haar_Cascades.xml')#เเปลงตรวจจับใบหน้าให้อยู่ใน face_cascade
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#แปลงรูปเป็นขาวดำ
 
 faces = face_cascade.detectMultiScale(gray, 1.3)   #ใช้ Haar Cascades ในการตรวจจับใบหน้า
